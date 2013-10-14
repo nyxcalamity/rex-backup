febab2158a06082398eb2c1ee949ed5aeb4b4bf9
Denys S <denys.sobchyshak@gmail.com>
2013-10-14 16:02:28 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 rex-backup
-==========
+----
+
+A collection of backup scripts that uses rsync and robocopy for it's routines.
+
+Requirements
+----
+
+- mounted shares
+- installed rsync or robocopy
+- installed tar or 7zip
+- installed python 3.3+
---
+++ scripts/config.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+    Copyright 2013 CRX Markets S.A.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+__author__ = "Denys Sobchyshak"
+__email__ = "denys.sobchyshak@gmail.com"
+
+class Context:
+    '''
+        Class that contains configuration strings.
+    '''
+
+    def __init__(self, backupDowntime, rotationPeriod, source=None, target=None, archive=True):
+        self.source = source
+        self.target = target
+        self.archive = archive
+        self.backupDowntime = backupDowntime
+        self.rotationPeriod = rotationPeriod
